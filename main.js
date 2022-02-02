e3a2b8b31f196411ea84edd287e3eed7b3594146
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-02-02 17:27:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ function preload(){}
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw(){}
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
 
-function take_snapshot(){
-    save('myFilterImage.png')
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("x =" + results[0].leftEye.pose.x + 50);
+        console.log("y =" + results[0].leftEye.pose.y);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
 }
