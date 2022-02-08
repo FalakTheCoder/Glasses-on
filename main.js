7789e3bf3bff7fc82e336aaaab9fd6245da80289
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-02-08 17:34:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload(){}
+eyeX=0;
+eyeY=0;
+
+function preload(){
+    glasses = loadImage('https://i.postimg.cc/L6Ls0r7n/glasses.png')
+}
 
 function setup(){
     canvas = createCanvas(300, 300);
@@ -18,11 +23,20 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("x =" + results[0].leftEye.pose.x + 50);
-        console.log("y =" + results[0].leftEye.pose.y);
+        eyeX = results[0].pose.rightEye.x - 25
+        eyeY = results[0].pose.rightEye.y - 20
+        console.log("eye x =" + eyeX);
+        console.log("eye y =" + eyeY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    image(glasses, eyeX, eyeY, 93, 50)
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
 }
